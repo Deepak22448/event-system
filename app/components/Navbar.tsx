1a064fe58af0d5e55c9cf055f65907f51aa3229c
deepak22448 <deepaksandeep33@gmail.com>
2023-09-14 21:22:45 +0530

    Update Navbar styling to use orange colors instead of gray colors, For better visibility on screen.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="h-16 flex w-full  items-center  px-4 text-gray-400 backdrop-blur-md fixed z-10 top-0  border-b border-b-gray-400">
+    <header className="h-16 flex w-full  items-center  px-4 text-orange-400 backdrop-blur-md fixed z-10 top-0  border-b border-b-orange-300">
       <nav className="flex w-full justify-between">
-        <Link href="/" className="text-2xl text-gray-100 font-extrabold">
+        <Link href="/" className="text-2xl text-orange-500 font-extrabold">
           EventTiz
         </Link>
 
         <ul className="flex gap-8 items-center">
-          <Link href="/login" className="hover:text-gray-100">
+          <Link href="/login" className="hover:text-orange-300">
             <li>Login</li>
           </Link>
-          <Link href="/register" className="hover:text-gray-100">
+          <Link href="/register" className="hover:text-orange-300">
             <li>Register</li>
           </Link>
         </ul>
