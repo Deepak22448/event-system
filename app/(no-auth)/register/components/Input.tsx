7b05a68f7d188db328390a6d758c725289657500
deepak22448 <deepaksandeep33@gmail.com>
2023-09-21 18:42:13 +0530

    [register/Input] Add optional placeholder prop for input fields

--- app/(no-auth)/register/components/Input.tsx
+++ app/(no-auth)/register/components/Input.tsx
@@ -7,12 +7,13 @@ interface Props {
   name: string;
   id: string;
   type: HTMLInputTypeAttribute;
-  iconPath: string;
+  iconPath?: string;
   label: string;
   autoComplete?: boolean;
   register: UseFormRegister<any>;
   isRequired?: boolean;
   error?: string | undefined;
+  placeholder?: string;
 }
 const Input: FC<Props> = ({
   type = "text",
@@ -23,28 +24,38 @@ const Input: FC<Props> = ({
   autoComplete = false,
   register,
   isRequired = false,
+  placeholder,
   error,
 }) => {
   return (
-    <div>
+    <div className="w-full">
       <label htmlFor={id}>
         {isRequired && "*"}
         {label}
+        {!isRequired && (
+          <span className="text-gray-400 ml-2">{"(Optional)"}</span>
+        )}
       </label>
       <div className="relative ">
-        <Image
-          src={iconPath}
-          alt="an icon"
-          width={30}
-          height={30}
-          className="absolute top-1/2 -translate-y-1/2 left-6 -translate-x-1/2 capitalize"
-        />
+        {iconPath && (
+          <Image
+            src={iconPath}
+            alt="an icon"
+            width={30}
+            height={30}
+            className="absolute top-1/2 -translate-y-1/2 left-6 -translate-x-1/2 capitalize"
+          />
+        )}
+
         <input
           {...register(name, { required: isRequired })}
           type={type}
           id={id}
+          placeholder={placeholder}
           name={name}
-          className="pl-12 border rounded outline-orange-400 py-3 w-full"
+          className={` ${
+            iconPath ? "pl-12" : "pl-2"
+          } border rounded outline-orange-400 py-3 w-full`}
           autoComplete={!autoComplete ? "off" : "on"}
         />
       </div>
